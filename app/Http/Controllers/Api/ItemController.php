227b0ca79935f476cb73dd057ca00c70d11acaf9
elena maksakova <elena.m@phpdev.org>
2024-01-17 17:24:00 +0300

    redis added

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Item;
+use Illuminate\Support\Facades\Cache;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        $items = Item::paginate(10); // Пагинация с 10 элементами на странице
+        $items = Cache::remember('items', 60*60, function () {
+            return Item::paginate(10);
+        });
 
         return response()->json($items, 200);
     }
